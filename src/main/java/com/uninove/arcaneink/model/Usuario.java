8458cd6c0e5d1095832cfcaa316ba6f6f82a3aef
Marina Tanaka <huttenloucher@gmail.com>
2025-12-01 16:14:59 -0300

    Adicionado descrição no Model e estilização da pagina usuario

--- src/main/java/com/uninove/arcaneink/model/Usuario.java
+++ src/main/java/com/uninove/arcaneink/model/Usuario.java
@@ -19,28 +19,66 @@ public class Usuario {
 
     private String tipo; // "cliente" ou "tatuador"
 
-    public Usuario() {}
+    private String descricao;
 
-    public Usuario(String nome, String email, String senha, String tipo) {
+    public Usuario() {
+    }
+
+    public Usuario(String nome, String email, String senha, String tipo, String descricao) {
         this.nome = nome;
         this.email = email;
         this.senha = senha;
         this.tipo = tipo;
+        this.descricao = descricao;
     }
 
     // GETTERS e SETTERS
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
+    // Getters e Setters
+    public Integer getId() {
+        return id;
+    }
 
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() {
+        return email;
+    }
 
-    public String getSenha() { return senha; }
-    public void setSenha(String senha) { this.senha = senha; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-    public String getTipo() { return tipo; }
-    public void setTipo(String tipo) { this.tipo = tipo; }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
